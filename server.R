6242441ab42c97b8d76d7f48965ed16cb04237d5
Walter <haomiaomeng@126.com>
2014-03-16 14:35:41 -0700

    Add more comments

--- server.R
+++ server.R
@@ -8,21 +8,21 @@ shinyServer(function(input, output) {
   i=as.integer(input$r)
   image(0:33, 0:13, -avgsig[[i]]-24, xlim=c(-0.5, 34), ylim=c(-3, 14.5), -avgsig[[1]]-24,
    col=colors, breaks=seq(0, 70, 10), main='Building Area', xlab='x', ylab='y'
-  )
+  ) #Average signal strength of different routers at each location.
   with(AccessPointLocations, points(x[-i], y[-i], pch=17, cex=1.4))
   with(AccessPointLocations, points(x[i], y[i], pch=17, col=5, cex=1.4))
   legend('topright', legend=c(paste('Router', i), 'Other Routers'), pch=17, col=c(5, 1))
  })
  output$signal=renderPlot({
   i=as.integer(input$r)
-  hx=floor(as.numeric(input$h$x)+0.5)
+  hx=floor(as.numeric(input$h$x)+0.5) #When mouse hover on 3.5 to 4.5, the actual point is 4.
   hy=floor(as.numeric(input$h$y)+0.5)
-  d=with(sub[[i]], circular(orientation[x==hx & y==hy]+90, units='degrees'))
-  s=with(sub[[i]], -signal[x==hx & y==hy]-24)
+  d=with(sub[[i]], circular(orientation[x==hx & y==hy]+90, units='degrees')) #Ajust orientation.
+  s=with(sub[[i]], -signal[x==hx & y==hy]-24) #Transform signal scale so that it's from 1 to 69.
   if (length(d)!=0) {
    windrose(d, s, breaks, main='', fill=colors)
   } else {
-   windrose(breaks, rep(1, 8), breaks, main='', fill=F)
+   windrose(breaks, rep(1, 8), breaks, main='', fill=F) #If NULL, draw a blank plot.
   }
  })
 })
